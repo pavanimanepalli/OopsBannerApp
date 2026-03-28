74550dad618d184307baefa4fa1ebeee1c961dd4
pavanimanepalli <pavanimanepalli3@gmail.com>
2026-03-28 17:02:48 +0530

    Add OOPSBanner class with banner output

---
+++ UC2.java
@@ -0,0 +1,16 @@
+public class OOPSBanner {
+
+    public static void main(String[] args) {
+
+        // Banner Lines
+        System.out.println(" *****  *****  *****  ***** ");
+        System.out.println(" **  **  **  **  **  **  **  ");
+        System.out.println(" **  **  **  **  **  **  **  ");
+        System.out.println(" **  **  ******  *****  ***** ");
+        System.out.println(" **  **  **  **  **     **  ");
+        System.out.println(" **  **  **  **  **     **  ");
+        System.out.println(" *****   **  **  **     ***** ");
+
+        System.out.println("\nWelcome to OOPSBanner application");
+    }
+}
